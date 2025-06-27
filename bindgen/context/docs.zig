82d813541ed757751543167d8557d3c4b759fa01
Simon Hartcher <simon@simonhartcher.com>
2025-06-27 23:36:01 +1000

    feat: start working on Godot bbcode custom elements

--- bindgen/context/docs.zig
+++ bindgen/context/docs.zig
@@ -1,16 +1,46 @@
+const Element = enum {
+    codeblocks,
+};
+
+fn writeElement(node: Node, context: ?*anyopaque) anyerror!bool {
+    const ctx: *Context = @alignCast(@ptrCast(context));
+    const el: Element = std.meta.stringToEnum(Element, try node.getName()) orelse return false;
+
+    return switch (el) {
+        .codeblocks => try writeCodeblocks(node, ctx),
+    };
+}
+
+fn writeCodeblocks(node: Node, ctx: *Context) anyerror!bool {
+    for (node.children.items) |child| {
+        const lang = try child.getName();
+        try ctx.writer.print("```{s}\n", .{lang});
+        try render(child, ctx);
+        try ctx.writer.writeAll("```\n");
+    }
+
+    return true;
+}
+
 pub fn convertDocsToMarkdown(allocator: Allocator, input: []const u8) ![]const u8 {
-    var doc = try BbcodeDocument.loadFromBuffer(allocator, input);
+    var doc = try Document.loadFromBuffer(allocator, input, .{});
     defer doc.deinit();
 
     var output = ArrayList(u8){};
-    try bbcodez.fmt.md.renderDocument(allocator, doc, output.writer(allocator).any());
+    try bbcodez.fmt.md.renderDocument(allocator, doc, output.writer(allocator).any(), .{
+        .write_element_fn = writeElement,
+    });
 
     return output.toOwnedSlice(allocator);
 }
 
+const render = bbcodez.fmt.md.render;
+
+const Node = bbcodez.Node;
+const Document = bbcodez.Document;
 const Allocator = std.mem.Allocator;
-const BbcodeDocument = bbcodez.Document;
 const ArrayList = std.ArrayListUnmanaged;
+const Context = bbcodez.fmt.md.WriteContext;
 
 const std = @import("std");
 const bbcodez = @import("bbcodez");
